b8177b5ee2e418d757074763aca4d8e131ea3650
jbite <jbie9057@gmail.com>
2024-12-13 16:42:06 +0800

    structure build

---
+++ Info/Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import contents.inform as pInfo
+st.title("Tech with Jacky")
+
+
+col1, col2 = st.columns(2)
+with col1:
+    st.image("images/myphoto.jpg", caption="Hi there",width=150)
+
+with col2:
+    st.write(f"""Phone: {pInfo.PHONE}\n
+Email: {pInfo.PERSONAL_EMAIL}
+             """)
---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="Data manager", page_icon=":material/edit:",layout="wide")
+home_page = st.Page("Info/Home.py", title="Home", icon=":material/home:",default=True)
+aws_page = st.Page("My_Learning_Journey/AWS.py", title="AWS", icon=":material/cloud:")
+python_page = st.Page("My_Learning_Journey/Python.py", title="Python", icon=":material/code:")
+ml_page = st.Page("My_Learning_Journey/Machine_Learning.py", title="Machine Learning", icon=":material/robot_2:")
+
+pg = st.navigation(
+    {   "Info": [home_page],
+        "My Learning Journey": [aws_page, python_page,ml_page]
+    }
+)
+pg.run()
