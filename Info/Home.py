6f70b283a7562183e647f72e93c73c0794b0bfb8
jbite <jbite9057>
2024-12-13 20:08:24 +0800

    info

--- Info/Home.py
+++ Info/Home.py
@@ -5,9 +5,13 @@ st.title("Tech with Jacky")
 
 col1, col2 = st.columns(2)
 with col1:
-    st.image("images/myphoto.jpg", caption="Hi there",width=150)
+    col3,col4 = st.columns([0.5,1])
+    with col3:
+        st.image("images/myphoto.jpg",width=150)
+    with col4:
+        st.markdown(f"""**Name**: Jacky Feng\n
+**Phone**: {pInfo.PHONE}\n
+**Email**: {pInfo.PERSONAL_EMAIL}""")
 
 with col2:
-    st.write(f"""Phone: {pInfo.PHONE}\n
-Email: {pInfo.PERSONAL_EMAIL}
-             """)
+    st.info("Hi, I am Jacky Feng, I am a associate consultant work in IT area over 10 years. ")
