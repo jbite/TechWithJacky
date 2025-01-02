62f250fc194ff9e84306d24652f99c9ce4e7f496
jbite <jbite9057>
2025-01-02 23:43:47 +0800

    LLM day2

--- My_Learning_Journey/Machine_Learning.py
+++ My_Learning_Journey/Machine_Learning.py
@@ -1,10 +1,28 @@
 import streamlit as st
 
+Day1Title = "Try LLM"
 Day1Content = """
 - Try Ollama locally
 - Complete a news summarize application with open API and reuqests
+"""
 
+Day2Title = "Compare to frontier"
+Day2Content = """
+- What are the frontiers of LLM model
+  - OpenAI
+  - Gemini
+  - Cohere
+  - Perplexity
+- What can LLM model do?
+  - synthesizeing information
+  - fleshing out a skeleton 
+  - coding
+- Compare to those models
 """
 st.title("LLM")
-st.markdown("*2025-01-01*")
+st.header(Day1Title)
+st.write("**2025-01-01**")
 st.write(Day1Content)
+
+
+st.title("")
